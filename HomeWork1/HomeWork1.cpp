04a06011758b3211cd0335d9846dc8d891a22e1e
Stepanov Nikita <vaprvsapr@mail.ru>
2021-02-10 19:21:54 +0300

    hello

--- HomeWork1/HomeWork1.cpp
+++ HomeWork1/HomeWork1.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main() {
     // put your code here
     float x, y, z;
+    cin >> x >> y >> z;
     float p = (x + y + z) / 2;
 
     float s = sqrt(p * (p - x) * (p - y) * (p - z));

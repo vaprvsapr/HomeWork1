9757b8860e6fb1d942c40046cb6d00c8c1b8ee1d
Stepanov Nikita <vaprvsapr@mail.ru>
2021-02-10 19:25:14 +0300

    commit commit

--- HomeWork1/HomeWork1.cpp
+++ HomeWork1/HomeWork1.cpp
@@ -3,9 +3,17 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+
+int to_do(int i) {
+    cout << i;
+}
+
 int main() {
     // put your code here
     float x, y, z;
+
+    cin >> x >> y >> z;
+
     float p = (x + y + z) / 2;
 
     float s = sqrt(p * (p - x) * (p - y) * (p - z));

d6ae1017109f8c323207ea03f3dba1a5e4477689
Stepanov Nikita <vaprvsapr@mail.ru>
2021-02-10 19:29:42 +0300

    zero

--- HomeWork1/HomeWork1.cpp
+++ HomeWork1/HomeWork1.cpp
@@ -1,21 +1,7 @@
-
-
 #include <iostream>
-#include <cmath>
+
 using namespace std;
 
-void nothing(int i) {
-    
-}
-
 int main() {
-    // put your code here
-    float x, y, z;
-    cin >> x >> y >> z;
-    float p = (x + y + z) / 2;
 
-    float s = sqrt(p * (p - x) * (p - y) * (p - z));
-
-    cout << s;
-    return 0;
 }

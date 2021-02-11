2649526ccb176d4c0da4489b9d09e9c1e4271d86
Stepanov Nikita <vaprvsapr@mail.ru>
2021-02-11 17:46:19 +0300

    ClassWork

--- HomeWork1/HomeWork1.cpp
+++ HomeWork1/HomeWork1.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
+#include <vector>
+
 
 using namespace std;
 
-int main() {
-	int n;
-	cin >> n;
-	cout << n;
+void PrintList(vector<int> v) {
+	for (auto element : v) {
+		cout << element << " ";
+	}
+}
 
-	int p;
-	cin >> p;
-	cout << p;
+int main() {
+
+	vector<int> a = { 1, 2, 3, 4, 5 };
+	vector<int> b = { 6, 7, 8, 9, 10 };
+	vector<int> c = a;
+
+	auto iter1 = a.rbegin();
+	auto iter2 = b.begin();
+
+	while (iter1 != a.rend()) {
+		*iter1 = *iter2;
+
+		iter1 = next(iter1);
+		iter2 = next(iter2);
+	}
+
+	b = c;
+
+	PrintList(a);
+	cout << endl;
+	PrintList(b);
 }
